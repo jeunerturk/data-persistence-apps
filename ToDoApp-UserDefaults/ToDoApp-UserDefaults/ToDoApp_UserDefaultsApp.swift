c64c57c816b2c293d058c3aaa6d8486fc84c7b0c
Umut Erturk <umutc.erturk@gmail.com>
2022-10-01 20:17:45 +0300

    initial commit

---
+++ ToDoApp-UserDefaults/ToDoApp-UserDefaults/ToDoApp_UserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp_UserDefaultsApp.swift
+//  ToDoApp-UserDefaults
+//
+//  Created by Umut Can Ertürk on 1.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp_UserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
